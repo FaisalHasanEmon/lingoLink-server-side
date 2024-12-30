0bb56f361cc529b8298a67a8bfd238502a115712
FaisalHasanEmon <faisalhasan5833@gmail.com>
2024-12-30 08:38:06 +0600

    Connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5500;
-require("dotenv").config;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Default response
 app.get("/", (req, res) => {
-  res.send({ Message: "It's working" });
+  res.send("Language Learners Heaven");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rmsmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const lingoLinkDB = client.db("lingoLink");
+    const tutorsCollection = lingoLinkDB.collection("tutors");
+    // Load all data
+    app.get("/tutors", async (req, res) => {
+      const result = await tutorsCollection.find().toArray();
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
