fcfc97fe986bca6e5493c3d1df84d48726174d0f
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-01-02 22:57:09 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const lingoLinkDB = client.db("lingoLink");
     const tutorsCollection = lingoLinkDB.collection("tutors");
@@ -130,6 +130,13 @@ async function run() {
       const result = await tutorsCollection.find(query).toArray();
       res.send(result);
     });
+    app.get("/tutors-images", async (req, res) => {
+      const images = await tutorsCollection
+        .find({}, { projection: { image: 1, _id: 0 } })
+        .toArray();
+
+      res.send(images);
+    });
     // Update My Tutorials
     app.put("/myTutorials", async (req, res) => {
       const updateTutorial = req.body;
@@ -200,10 +207,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -212,5 +219,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  // console.log(`Server is running on port: ${port}`);
 });
